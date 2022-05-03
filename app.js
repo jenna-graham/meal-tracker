7e86cdd0b7ec654d34017121047ab772ba2807e5
jenna-graham <jclayto2@gmail.com>
2022-05-02 18:30:37 -0700

    added a form in HTML, got Dom elements, added event listener for ingredient form submit button. checked by console.logging ingredient form names (3 of them)

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 // import functions and grab DOM elements
+const ingredientForm = document.getElementById('ingredient-form');
+
 
 // let state
+// let ingredients = 0;
+
 
 // set event listeners 
+ingredientForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const ingredientData = new FormData(ingredientForm);
+    console.log(ingredientData.get('food-name'));
+    console.log(ingredientData.get('quantity'));
+    console.log(ingredientData.get('measurement'));
+
+
+    // let ingredient = {
+    //     name: ingredientData.get('food-name'),
+    //     quantity: ingredientData.get('quantity'),
+    //     measurement: ingredientData.get('measurement'),
+    // };    
+
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
